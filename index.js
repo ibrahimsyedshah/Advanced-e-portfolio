21176a6ed001edbeec4423f5d3390e9a9031d7ac
Ibrahim <ibrahim.syedshah@gmail.com>
2023-07-10 16:40:51 +1000

    finished responsiveness and final touches

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;    
+    const y = event.clientY * scaleFactor;    
+
+    for(let i = 0; i < shapes.length; i++) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+    }
+}
 
 function toggleContrast() {
     console.log("aaaaaaaaaaã");
@@ -42,3 +55,4 @@ function toggleModal() {
     }
     isModalOpen = !isModalOpen;
 }
+
